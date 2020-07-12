13262e646f6bfbaac24703840e72bc74cc8d7f3c
RomanK <namor925@gmail.com>
2020-07-12 17:21:37 +0700

    First commit. Create structure

---
+++ src/test/java/com/katzendorn/resume/BeyondAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katzendorn.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
